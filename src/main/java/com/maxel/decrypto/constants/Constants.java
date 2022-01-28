aa68a16d770d058c95c1c94c58502cc727086ffc
Maxel-Uds <maxellopes32@gmail.com>
2022-01-28 00:14:04 -0400

    Adicionando 48 caracteres diferentes aos random e os numerais as normais

--- src/main/java/com/maxel/decrypto/constants/Constants.java
+++ src/main/java/com/maxel/decrypto/constants/Constants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Constants {
 
-    public static final String NORMALLETTER = "qwertyuiopasdfghjklçzxcvbnmáéíóìàôõâãê";
-    public static final String RANDOMLETTER = "!9$%3@?/*8&56-:;0274^~[]{}|JSbGhjVU1><!9$%3@?/*8&56-:;0274^~[]{}|JSbGhjVU1><";
+    public static final String NORMALLETTER = "0123456789qwertyuiopasdfghjklçzxcvbnmáéíóìàôõâãê";
+    public static final String RANDOMLETTER = "!9ՋՌՍ$%3@ÆØßՄՅՆՇ?/*8&56-:ƏƩƪƱȣȸȹɷɸʘՀՁՂՃ;027¥§4^~[ΦԻDԾԿ]{}|JÇObGhjVU1>EBsHguvƒŠŒŽŸ¢£µ¿<ÇՈՉՊՎ=MRՐՑ";
     public static final List<Character> CHARSUPPER = Arrays.asList('T', 'F', 'N', 'L', 'Q', 'P', 'Y', 'I');
 }
