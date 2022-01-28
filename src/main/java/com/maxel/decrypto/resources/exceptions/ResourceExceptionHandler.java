71befbd253a7416c39ae01b19806630d189bc6db
Maxel-Uds <maxellopes32@gmail.com>
2022-01-28 13:27:05 -0400

    Tratando os erros 422 que podem aparecer

---
+++ src/main/java/com/maxel/decrypto/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.maxel.decrypto.resources.exceptions;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @Value("${message.exception.argument.not.valid}")
+    private String argumentNotValidMessage;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest req) {
+        ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de Validação", argumentNotValidMessage, req.getRequestURI());
+
+        for (FieldError field : e.getBindingResult().getFieldErrors()) {
+            err.addError(field.getField(), field.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+    }
+}
---
+++ src/main/java/com/maxel/decrypto/resources/exceptions/ValidationError.java
@@ -0,0 +1,21 @@
+package com.maxel.decrypto.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+
+    List<FieldMessage> errors = new ArrayList<FieldMessage>();
+
+    public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+        super(timestamp, status, error, message, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
